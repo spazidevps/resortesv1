ab8447f451ec4921f300d3d7ef9b5a48bf66a411
spazidevps <spazidevps@proton.me>
2024-02-02 11:04:09 -0600

    se modifico para que mantenga peso atura y ancho actualziados

--- app.py
+++ app.py
@@ -277,20 +277,19 @@ def get_combinaciones():
 
 @app.route('/filtrar_resultados', methods=['POST'])
 def filtrar_resultados():
-    # Obtener los valores del formulario
+      # Obtener los valores del formulario
     peso_objetivo = float(request.form['peso'])
     altura = float(request.form['altura']) * 100  # Convertir a cm
+    ancho = float(request.form.get('ancho', 0))  # Asegurar que se captura el ancho, con un valor predeterminado si es necesario
     vueltas = calcular_vueltas(altura)
  # Obtener tipos de resortes seleccionados
     tipos_seleccionados = request.form.getlist('resortes_disponibles')
-    # Filtrar los resortes basándose en los tipos seleccionados
+       # Filtrar los resortes basándose en los tipos seleccionados
     resortes_filtrados = [resorte for resorte in resortes if resorte['tipo'] in tipos_seleccionados]
-
 # Calcular combinaciones con los resortes filtrados
     combinaciones_filtradas = []
     for num_resortes in [1, 2]:
         combinaciones_filtradas.extend(encontrar_combinaciones(peso_objetivo, vueltas, resortes_filtrados, num_resortes))
-
 # Calcular opciones adicionales
     opciones_adicionales = {
         num_resortes: {
@@ -298,16 +297,18 @@ def filtrar_resultados():
             'texto_boton': f"Ver resultados para {num_resortes} resortes"
         } for num_resortes in range(1, 6)
     }
-
 # Crear un conjunto de tipos únicos de resortes
     tipos_unicos = {resorte['tipo'] for resorte in resortes}
-
  # Renderizar la plantilla con los resultados filtrados y los tipos únicos
     return render_template('resultados.html',
                            combinaciones=combinaciones_filtradas,
                            opciones=opciones_adicionales,
                            tipos_unicos=tipos_unicos,
-                           tipos_seleccionados=tipos_seleccionados)
+                           tipos_seleccionados=tipos_seleccionados,
+                           peso=peso_objetivo,
+                           altura=altura / 100,  # Convertir de nuevo a metros para la visualización
+                           ancho=ancho)  # Incluir el ancho capturado para la visualización
+
 
 
 
